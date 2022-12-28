7c990cece7f17012f94e0e27be23426cad1c8f9b
Felipe <fvcalderan@gmail.com>
2022-12-28 11:16:59 -0300

    feat: DME and XPDR

--- fsradio.ino
+++ fsradio.ino
@@ -5,19 +5,22 @@
 /* LCD Display */
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-/* Keypad */
+/* keypad */
 uint16_t last_btn = 1023;
 uint64_t last_millis;
 
-/* Serial */
+/* serial */
 String last_serial_data = "";
 
-/* Mode */
+/* mode */
 uint8_t mode = 0;
-const uint8_t MAX_MODE = 3;
+const uint8_t MAX_MODE = 16;
 /*
- * 0: COM1 MHz | 1: COM1 KHz
- * 2: COM2 MHz | 3: COM2 KHz
+ *  0: COM1  MHz |  1: COM1  KHz |  2: COM2  MHz |  3: COM2  KHz
+ *  4: NAV1  MHz |  5: NAV1  KHz |  6: NAV2  MHz |  7: NAV2  KHz
+ *  8: ADF   100 |  9: ADF   010 | 10: ADF   001 | 11: ADF   DEC
+ * 12: DME  SWAP | 13: XPDR 1000 | 14: XPDR 0100 | 14: XPDR 0010
+ * 16: XPDR 0001 |
  */
 
 /* clear the LCD screen */
@@ -40,27 +43,106 @@ void lcd_blink(int x, int y)
 /* blink screen on different spots depending on the mode */
 void blink_mode()
 {
+    /* COM 1 */
     if      (mode == 0) lcd_blink(0, 1);
     else if (mode == 1) lcd_blink(5, 1);
+
+    /* COM 2 */
     else if (mode == 2) lcd_blink(0, 1);
     else if (mode == 3) lcd_blink(5, 1);
+
+    /* NAV 1 */
+    if      (mode == 4) lcd_blink(0, 1);
+    else if (mode == 5) lcd_blink(5, 1);
+
+    /* NAV 2 */
+    else if (mode == 6) lcd_blink(0, 1);
+    else if (mode == 7) lcd_blink(5, 1);
+
+    /* ADF */
+    else if (mode ==  8) lcd_blink( 6, 1);
+    else if (mode ==  9) lcd_blink( 7, 1);
+    else if (mode == 10) lcd_blink( 8, 1);
+    else if (mode == 11) lcd_blink(10, 1);
+
+    /* DME */
+    else if (mode == 12) lcd_blink(5, 0);
+
+    /* XPDR */
+    else if (mode == 13) lcd_blink(6, 1);
+    else if (mode == 14) lcd_blink(7, 1);
+    else if (mode == 15) lcd_blink(8, 1);
+    else if (mode == 16) lcd_blink(9, 1);
+
 }
 
 /* buttons*/
 void btn_up()
 {
+    /* COM 1 */
     if      (mode == 0) Serial.println("COM1_MUP");
     else if (mode == 1) Serial.println("COM1_KUP");
+
+    /* COM 2 */
     else if (mode == 2) Serial.println("COM2_MUP");
     else if (mode == 3) Serial.println("COM2_KUP");
+
+    /* NAV 1 */
+    else if (mode == 4) Serial.println("NAV1_MUP");
+    else if (mode == 5) Serial.println("NAV1_KUP");
+
+    /* NAV 2 */
+    else if (mode == 6) Serial.println("NAV2_MUP");
+    else if (mode == 7) Serial.println("NAV2_KUP");
+
+    /* ADF */
+    else if (mode ==  8) Serial.println("ADF_100UP");
+    else if (mode ==  9) Serial.println("ADF_010UP");
+    else if (mode == 10) Serial.println("ADF_001UP");
+    else if (mode == 11) Serial.println("ADF_DECUP");
+
+    /* DME */
+    else if (mode == 12) /* DME selected -> pass */;
+
+    /* XPDR */
+    else if (mode == 13) Serial.println("XPDR_1000UP");
+    else if (mode == 14) Serial.println("XPDR_0100UP");
+    else if (mode == 15) Serial.println("XPDR_0010UP");
+    else if (mode == 16) Serial.println("XPDR_0001UP");
 }
 
 void btn_down()
 {
+    /* COM 1 */
     if      (mode == 0) Serial.println("COM1_MDN");
     else if (mode == 1) Serial.println("COM1_KDN");
+
+    /* COM 2 */
     else if (mode == 2) Serial.println("COM2_MDN");
     else if (mode == 3) Serial.println("COM2_KDN");
+
+    /* NAV 1 */
+    if      (mode == 4) Serial.println("NAV1_MDN");
+    else if (mode == 5) Serial.println("NAV1_KDN");
+
+    /* NAV 2 */
+    if      (mode == 6) Serial.println("NAV2_MDN");
+    else if (mode == 7) Serial.println("NAV2_KDN");
+
+    /* ADF */
+    else if (mode ==  8) Serial.println("ADF_100DN");
+    else if (mode ==  9) Serial.println("ADF_010DN");
+    else if (mode == 10) Serial.println("ADF_001DN");
+    else if (mode == 11) Serial.println("ADF_DECDN");
+
+    /* DME */
+    else if (mode == 12) /* DME selected -> pass */;
+
+    /* XPDR */
+    else if (mode == 13) Serial.println("XPDR_1000DN");
+    else if (mode == 14) Serial.println("XPDR_0100DN");
+    else if (mode == 15) Serial.println("XPDR_0010DN");
+    else if (mode == 16) Serial.println("XPDR_0001DN");
 }
 
 void btn_right()
@@ -77,8 +159,13 @@ void btn_left()
 
 void btn_select()
 {
-    if      (mode < 2) Serial.println("COM1_SWP");
-    else if (mode < 4) Serial.println("COM2_SWP");
+    if      (mode <  2) Serial.println("COM1_SWP");
+    else if (mode <  4) Serial.println("COM2_SWP");
+    else if (mode <  6) Serial.println("NAV1_SWP");
+    else if (mode <  8) Serial.println("NAV2_SWP");
+    else if (mode < 12) /* ADF selected -> pass */;
+    else if (mode < 13) Serial.println("DME_TGL");
+    else if (mode < 17) /* XPDR selected -> pass */;
 }
 
 /* read buttons */
@@ -88,53 +175,97 @@ void read_buttons()
     uint64_t current_millis = millis();
 
     /* check for button click (with error margin) */
-    if (abs(btn - last_btn) > 50) {
-        /* avoid debounce */
-        if (current_millis - last_millis > 200) {
-            if (btn < 90) {
-                btn_right();
-            } else if (btn < 200) {
-                btn_up();
-            } else if (btn < 400) {
-                btn_down();
-            } else if (btn < 600) {
-                btn_left();
-            } else if (btn < 800) {
-                btn_select();
-            }
-            last_btn = btn;
-            last_millis = current_millis;
-        }
-    }
+    if (abs(btn - last_btn) <= 50) return;
+
+    /* avoid debounce */
+    if (current_millis - last_millis <= 200) return;
+    if      (btn <  90) btn_right();
+    else if (btn < 200) btn_up();
+    else if (btn < 400) btn_down();
+    else if (btn < 600) btn_left();
+    else if (btn < 800) btn_select();
+    last_btn = btn;
+    last_millis = current_millis;
 }
 
 /* read from serial */
 void read_serial()
 {
-    if (Serial.available() > 0) {
-        /* read the incoming text */
-        String serial_data = Serial.readStringUntil('\n');
+    if (Serial.available() < 1) return; /* Nothing to read */
+    String serial_data = Serial.readStringUntil('\n'); /* incoming text */
+    if (serial_data == last_serial_data) return; /* Nothing new received */
 
-        /* if something new was received, display it */
-        if (serial_data != last_serial_data) {
-            if (mode < 2) {
-                lcd_clear();
-                lcd.setCursor(0, 0);
-                lcd.print("COM1: ");
-                lcd.print(serial_data);
-                lcd.setCursor(0, 1);
-                lcd.print(" MHz  KHz");
-                last_serial_data = serial_data;
-            } else if (mode < 4) {
-                lcd_clear();
-                lcd.setCursor(0, 0);
-                lcd.print("COM2: ");
-                lcd.print(serial_data);
-                lcd.setCursor(0, 1);
-                lcd.print(" MHz  KHz");
-                last_serial_data = serial_data;
-            }
-        }
+    /* if something new was received, display it */
+
+    /* COM 1 */
+    if (mode < 2) {
+        lcd_clear();
+        lcd.setCursor(0, 0);
+        lcd.print("COM1: ");
+        lcd.print(serial_data);
+        lcd.setCursor(0, 1);
+        lcd.print(" MHz  KHz");
+        last_serial_data = serial_data;
+
+    /* COM 2 */
+    } else if (mode < 4) {
+        lcd_clear();
+        lcd.setCursor(0, 0);
+        lcd.print("COM2: ");
+        lcd.print(serial_data);
+        lcd.setCursor(0, 1);
+        lcd.print(" MHz  KHz");
+        last_serial_data = serial_data;
+
+    /* NAV 1 */
+    } else if (mode < 6) {
+        lcd_clear();
+        lcd.setCursor(0, 0);
+        lcd.print("NAV1: ");
+        lcd.print(serial_data);
+        lcd.setCursor(0, 1);
+        lcd.print(" MHz  KHz");
+        last_serial_data = serial_data;
+
+    /* NAV 2 */
+    } else if (mode < 8) {
+        lcd_clear();
+        lcd.setCursor(0, 0);
+        lcd.print("NAV2: ");
+        lcd.print(serial_data);
+        lcd.setCursor(0, 1);
+        lcd.print(" MHz  KHz");
+        last_serial_data = serial_data;
+
+    /* ADF */
+    } else if (mode < 12) {
+        lcd_clear();
+        lcd.setCursor(0, 0);
+        lcd.print("ADF: ");
+        lcd.print(serial_data);
+        last_serial_data = serial_data;
+
+    /* DME */
+    } else if (mode < 13) {
+        lcd_clear();
+        lcd.setCursor(0, 0);
+        lcd.print("DME:  #");
+        lcd.print(serial_data.substring(0, 1));
+        lcd.setCursor(0, 1);
+        lcd.print(serial_data.substring(1, 5));
+        lcd.print(" nm ");
+        lcd.print(serial_data.substring(5));
+        lcd.print(" KT");
+        last_serial_data = serial_data;
+        /* FIX CURSOR POSITION BUG ON MODE CHANGE */
+
+    /* XPDR */
+    } else if (mode < 17) {
+        lcd_clear();
+        lcd.setCursor(0, 0);
+        lcd.print("XPDR: ");
+        lcd.print(serial_data);
+        last_serial_data = serial_data;
     }
 }
 
@@ -142,7 +273,7 @@ void read_serial()
 /* setup function */
 void setup()
 {
-    /* Write stuff to the LCD screen */
+    /* write stuff to the LCD screen */
     lcd.begin(16, 2);
     lcd.setCursor(0, 0);
     lcd.print("FVC FS2004 Radio");
@@ -150,7 +281,7 @@ void setup()
     lcd.print(" V1.0 [13/11/22]");
     lcd_blink(0, 1);
 
-    /* Setup Serial port with USB baud rate */
+    /* setup Serial port with USB baud rate */
     Serial.begin(115200);
 }
 
--- fsradio.lua
+++ fsradio.lua
@@ -7,7 +7,7 @@ handshake = 0
 
 -- operation mode
 mode = 0
-MAX_MODE = 3
+MAX_MODE = 16
 
 -- COM Port Setup
 file = io.open(port_file, "r")
@@ -51,62 +51,158 @@ function get_serial_data(com_port, datastring, length)
     -- MODE
 	if (string.find(datastring, "MODE_UP")) then
 		mode = math.min(MAX_MODE, mode + 1) -- Mode Up
-	end
-	if (string.find(datastring, "MODE_DN")) then
+    elseif (string.find(datastring, "MODE_DN")) then
 		mode = math.max(0, mode - 1) -- Mode Down
-	end
 
     -- COM 1
-	if (string.find(datastring, "COM1_MUP")) then
+    elseif (string.find(datastring, "COM1_MUP")) then
 		ipc.control(65637, 0)   -- MHz Up
-	end
-	if (string.find(datastring, "COM1_MDN")) then
+    elseif (string.find(datastring, "COM1_MDN")) then
 		ipc.control(65636, 0)   -- MHz Down
-	end
-	if (string.find(datastring, "COM1_KUP")) then
+    elseif (string.find(datastring, "COM1_KUP")) then
 		ipc.control(65639, 0)   -- KHz Up
-	end
-	if (string.find(datastring, "COM1_KDN")) then
+    elseif (string.find(datastring, "COM1_KDN")) then
 		ipc.control(65638, 0)   -- KHz Down
-	end
-	if (string.find(datastring, "COM1_SWP")) then
+    elseif (string.find(datastring, "COM1_SWP")) then
 		ipc.control(66372, 0)   -- Standby Swap
-	end
 
     -- COM 2
-	if (string.find(datastring, "COM2_MUP")) then
+    elseif (string.find(datastring, "COM2_MUP")) then
 		ipc.control(66437, 0)   -- MHz Up
-	end
-	if (string.find(datastring, "COM2_MDN")) then
+    elseif (string.find(datastring, "COM2_MDN")) then
 		ipc.control(66436, 0)   -- MHz Down
-	end
-	if (string.find(datastring, "COM2_KUP")) then
+    elseif (string.find(datastring, "COM2_KUP")) then
 		ipc.control(66440, 0)   -- KHz Up
-	end
-	if (string.find(datastring, "COM2_KDN")) then
+    elseif (string.find(datastring, "COM2_KDN")) then
 		ipc.control(66438, 0)   -- KHz Down
-	end
-	if (string.find(datastring, "COM2_SWP")) then
+    elseif (string.find(datastring, "COM2_SWP")) then
 		ipc.control(66444, 0)   -- Standby Swap
+
+    -- NAV 1
+    elseif (string.find(datastring, "NAV1_MUP")) then
+		ipc.control(65641, 0)   -- MHz Up
+    elseif (string.find(datastring, "NAV1_MDN")) then
+		ipc.control(65640, 0)   -- MHz Down
+    elseif (string.find(datastring, "NAV1_KUP")) then
+		ipc.control(65643, 0)   -- KHz Up
+    elseif (string.find(datastring, "NAV1_KDN")) then
+		ipc.control(65642, 0)   -- KHz Down
+    elseif (string.find(datastring, "NAV1_SWP")) then
+		ipc.control(66448, 0)   -- Standby Swap
+
+    -- NAV 2
+    elseif (string.find(datastring, "NAV2_MUP")) then
+		ipc.control(65645, 0)   -- MHz Up
+    elseif (string.find(datastring, "NAV2_MDN")) then
+		ipc.control(65644, 0)   -- MHz Down
+    elseif (string.find(datastring, "NAV2_KUP")) then
+		ipc.control(65647, 0)   -- KHz Up
+    elseif (string.find(datastring, "NAV2_KDN")) then
+		ipc.control(65646, 0)   -- KHz Down
+    elseif (string.find(datastring, "NAV2_SWP")) then
+		ipc.control(66452, 0)   -- Standby Swap
+
+    -- ADF
+    elseif (string.find(datastring, "ADF_100UP")) then
+		ipc.control(65648, 0)   -- 100 Up
+    elseif (string.find(datastring, "ADF_100DN")) then
+		ipc.control(65666, 0)   -- 100 Down
+    elseif (string.find(datastring, "ADF_010UP")) then
+		ipc.control(65649, 0)   -- 010 Up
+    elseif (string.find(datastring, "ADF_010DN")) then
+		ipc.control(65667, 0)   -- 010 Down
+    elseif (string.find(datastring, "ADF_001UP")) then
+		ipc.control(65650, 0)   -- 001 Up
+    elseif (string.find(datastring, "ADF_001DN")) then
+		ipc.control(65668, 0)   -- 001 Down
+    elseif (string.find(datastring, "ADF_DECUP")) then
+		ipc.control(66454, 0)   -- DEC Up
+    elseif (string.find(datastring, "ADF_DECDN")) then
+		ipc.control(66453, 0)   -- DEC Down
+
+    -- DME
+    elseif (string.find(datastring, "DME_TGL")) then
+        ipc.control(65789 - ipc.readSD(0x378), 0) -- DME Toggle
+
+    -- XPDR
+    elseif (string.find(datastring, "XPDR_1000UP")) then
+		ipc.control(65651, 0)   -- 1000 Up
+    elseif (string.find(datastring, "XPDR_1000DN")) then
+		ipc.control(66455, 0)   -- 1000 Down
+    elseif (string.find(datastring, "XPDR_0100UP")) then
+		ipc.control(65652, 0)   -- 0100 Up
+    elseif (string.find(datastring, "XPDR_0100DN")) then
+		ipc.control(66456, 0)   -- 0100 Down
+    elseif (string.find(datastring, "XPDR_0010UP")) then
+		ipc.control(65653, 0)   -- 0010 Up
+    elseif (string.find(datastring, "XPDR_0010DN")) then
+		ipc.control(66457, 0)   -- 0010 Down
+    elseif (string.find(datastring, "XPDR_0001UP")) then
+		ipc.control(65654, 0)   -- 0001 Up
+    elseif (string.find(datastring, "XPDR_0001DN")) then
+		ipc.control(66458, 0)   -- 0001 Down
 	end
 
-    ipc.display(datastring.." "..mode)
+    -- ipc.display(datastring.." "..mode)
 end
 
 -- Send data to Arduino
 function set_serial_data()
     -- Choose which information to send
-    if mode < 2 then        -- COM 1
+
+    -- COM 1
+    if mode < 2 then
         raw = string.sub(string.format("%x", ipc.readSD(0x311A)), -4)
         info = "1"..string.sub(raw, 1, 2).."."..string.sub(raw, -2).."\n"
-    elseif mode < 4 then    -- COM 2
+
+    -- COM 2
+    elseif mode < 4 then
         raw = string.sub(string.format("%x", ipc.readSD(0x311C)), -4)
         info = "1"..string.sub(raw, 1, 2).."."..string.sub(raw, -2).."\n"
+
+    -- NAV 1
+    elseif mode < 6 then
+        raw = string.sub(string.format("%x", ipc.readSD(0x311E)), -4)
+        info = "1"..string.sub(raw, 1, 2).."."..string.sub(raw, -2).."\n"
+
+    -- NAV 2
+    elseif mode < 8 then
+        raw = string.sub(string.format("%x", ipc.readSD(0x3120)), -4)
+        info = "1"..string.sub(raw, 1, 2).."."..string.sub(raw, -2).."\n"
+
+    -- ADF
+    elseif mode < 12 then
+        raw = string.sub(string.format("%x", ipc.readSD(0x034C)), -3)
+        raw_dec = string.sub(string.format("%x", ipc.readSD(0x0356)), -1)
+        info = "0"..raw.."."..raw_dec.."\n"
+
+    -- DME
+    elseif mode < 13 then
+        dme = ipc.readSD(0x0378)
+        if dme == 1 then
+            dist = string.format("%03.1f", ipc.readSW(0x0300)/10)
+            knots = string.format("%03d", ipc.readSW(0x0302)/10)
+        else
+            dist = string.format("%03.1f", ipc.readSW(0x0306)/10)
+            knots = string.format("%03d", ipc.readSW(0x0308)/10)
+        end
+        -- FIX CURSOR POSITION BUG ON MODE CHANGE
+                                      -- 01234567
+        info = dme..dist..knots.."\n" -- ABB.BCCC
+
+    -- XPDR
+    elseif mode < 17 then
+        raw = string.format("%x", ipc.readSD(0x0354))
+        info = string.sub(raw, -4).."\n"
+
     end
+
+    -- ipc.display(info)
+
     -- Send information
     com.write(com_port, info)
 end
 
 -- Events
-event.com(com_port, 50,1, "get_serial_data")
+event.com(com_port, 50, 1, "get_serial_data")
 event.timer(100, "set_serial_data")
